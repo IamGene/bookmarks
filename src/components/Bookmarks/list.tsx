83ea7eec7c5b7a12b6ea4e146ed5a1c251ee0900
ZBJ <695378805@qq.com>
2026-01-07 01:16:10 +0800

    1.711

--- src/components/Bookmarks/list.tsx
+++ src/components/Bookmarks/list.tsx
@@ -213,8 +213,8 @@ function BookmarksPages(props: BookmarksPageProps) {
        console.log("href last", last);
      } */
     //else {} //是首页
-    // console.log("---------->href", window.location.href);
-
+    console.log("---------->href", window.location.href);
+    console.log("---------->href index", window.location.href.indexOf('/bookmarks') === -1);
     console.log("==============>history.replace('/bookmarks');");
     if (window.location.href.indexOf('/bookmarks') === -1) {
       console.log("---------->history.replace('/bookmarks');");
